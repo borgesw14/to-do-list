96529839709c8cd8cdb92e2455bcb7f20b969a62
William Borges <borgesw14@gmail.com>
2021-10-20 21:59:16 -0400

    form for new item built

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container has-text-centered">
+    <h1 class="title">To-Do List:</h1>
+    <hr />
+    <!-- Form -->
+    <div class="container has-text-left">
+      <div class="field">
+        <label for="" class="label">Category</label>
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Projects</option>
+              <option selected>Discrete</option>
+              <option>Web Dev</option>
+              <option>Algo</option>
+              <option>Cyber Security</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div class="field">
+        <label class="label">Description</label>
+        <div class="control is-expanded">
+          <input type="text" class="input" placeholder="Do some coding...." />
+        </div>
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <button class="button is-info">Save</button>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
+  margin-top: 3rem;
+  max-width: 80%;
 }
 </style>
