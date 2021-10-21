1b6ca03f5f345a0128203fefd2c30fbc9be15c29
William Borges <borgesw14@gmail.com>
2021-10-21 09:54:16 -0400

    post functionality from front end working

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,9 +8,10 @@
         <label for="" class="label">Category</label>
         <p class="control">
           <span class="select">
-            <select>
+            <select v-model="category">
+              <option disabled value="">Please Select</option>
               <option>Projects</option>
-              <option selected>Discrete</option>
+              <option>Discrete</option>
               <option>Web Dev</option>
               <option>Algo</option>
               <option>Cyber Security</option>
@@ -22,13 +23,34 @@
       <div class="field">
         <label class="label">Description</label>
         <div class="control is-expanded">
-          <input type="text" class="input" placeholder="Do some coding...." />
+          <input
+            type="text"
+            class="input"
+            placeholder="Do some coding...."
+            v-model="description"
+          />
         </div>
       </div>
 
       <div class="field">
         <p class="control">
-          <button class="button is-info">Save</button>
+          <button class="button is-info" @click="saveItem">Save</button>
+        </p>
+      </div>
+    </div>
+    <hr />
+    <div class="notification" v-for="(item, i) in items" :key="item._id">
+      <div class="columns">
+        <p class="column is-narrow">
+          <span class="tag is-primary">
+            {{ i + 1 }}
+          </span>
+        </p>
+        <p class="column">
+          {{ item.description }}
+        </p>
+        <p class="column">
+          <span class="is-narrow">{{ item.category }}</span>
         </p>
       </div>
     </div>
@@ -36,9 +58,32 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      items: [],
+      description: "",
+      category: "",
+    };
+  },
+  async mounted() {
+    const response = await axios.get("api/toDoListItems/");
+    this.items = response.data;
+  },
+  methods: {
+    async saveItem() {
+      const response = await axios.post("api/toDoListItems/", {
+        description: this.description,
+        category: this.category,
+      });
+      this.item.push(response.data);
+      this.description = "";
+      this.category = "";
+    },
+  },
 };
 </script>
 
